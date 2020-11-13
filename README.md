3bffbb0e18ea4ae7ca40532fc0f80ace6cbb5e0c
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2020-11-12 23:02:36 -0500

    completed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Inventory-App
+# Lulu's Boutique
+# A Simple Inventory-App
+
+## 🐶🐶 Description
+
+> Lulu's Boutique is a very simple JS inventory app for tracking product inventory.
---
+++ js/script.js
@@ -0,0 +1,44 @@
+class LuluBoutique {
+  constructor(price) {
+    this.price = price;
+    this.item = [];
+  }
+
+  Inventory(items = null) {
+    const itemName = items;
+    const price = this.price;
+    const itemAmount = this.itemAmount;
+    if (items !== null) {
+      this.item.push({ items: items, price: price, itemAmount: itemAmount });
+    }
+  }
+}
+
+class Clothes extends LuluBoutique {
+  constructor(price, itemAmount) {
+    super(price);
+    this.itemAmount = itemAmount;
+  }
+}
+
+class Snacks extends PetBoutique {
+  constructor(price, itemAmout) {
+    super(price);
+    this.itemAmount = itemAmout;
+  }
+}
+
+const fallSweaters = new Clothes();
+fallSweaters.price = 18.29;
+fallSweaters.itemAmount = 42;
+fallSweaters.item;
+fallSweaters.Inventory("sweaters");
+
+console.log(fallSweaters);
+
+const artisanSnacks = new Snacks();
+artisanSnacks.price = 4.49;
+artisanSnacks.itemAmount = 62;
+artisanSnacks.Inventory("artisanSnacks");
+
+console.log(artisanSnacks);
